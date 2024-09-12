e4124861339644c58f98736c716404d16062d4d9
aleongit <aleix.leon@gmail.com>
2024-09-12 10:36:43 +0000

    feat: add new odoo module

---
+++ tutorials/estate/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# tutorial
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'license': 'AGPL-3',
+    'depends': ['base'],
+    'author': "aleon",
+    'category': 'Test',
+    'description': "Description of Real Estate",
+    # data files always loaded at installation
+    # 'data': ['views/mymodule_view.xml',],
+    # data files containing optionally loaded demonstration data
+    # 'demo': ['demo/demo_data.xml',],
+}
