083adece7b2071776ff3fed92d2f73c021d087bd
aleongit <aleix.leon@gmail.com>
2024-09-12 14:45:45 +0000

    feat: add access security to the data in model

--- tutorials/estate/__manifest__.py
+++ tutorials/estate/__manifest__.py
@@ -9,7 +9,7 @@
     'category': 'Test',
     'description': "Description of Real Estate",
     # data files always loaded at installation
-    # 'data': ['views/mymodule_view.xml',],
+    'data': ['security/ir.model.access.csv',],
     # data files containing optionally loaded demonstration data
     # 'demo': ['demo/demo_data.xml',],
 }
