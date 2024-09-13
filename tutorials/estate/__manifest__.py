54495dd37faf2b78f9404c721a923e885e24543c
aleongit <aleix.leon@gmail.com>
2024-09-13 06:50:15 +0000

    feat: add menus

--- tutorials/estate/__manifest__.py
+++ tutorials/estate/__manifest__.py
@@ -10,8 +10,12 @@
     'description': "Description of Real Estate",
     # data files always loaded at installation
     'data': [
-        'security/ir.model.access.csv',
+        # data
+        # views
         'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+        # access
+        'security/ir.model.access.csv',
     ],
     # data files containing optionally loaded demonstration data
     # 'demo': ['demo/demo_data.xml',],
