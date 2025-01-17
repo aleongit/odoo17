d4658c7c49970ec57a3a2d0d8a87ff5b6100343f
aleongit <aleix.leon@gmail.com>
2025-01-17 16:15:00 +0000

    feat: add estate_account module

---
+++ tutorials/estate_account/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# tutorial
+{
+    'name': "Real Estate Account",
+    'version': '1.0',
+    'license': 'AGPL-3',
+    'depends': ['account'],
+    'author': "aleon",
+    'category': 'Test',
+    'description': "Real Estate Account",
+    # data files always loaded at installation
+    'data': [
+        # data
+        # views
+        # 'views/estate_property_views.xml',
+        # access
+        # 'security/ir.model.access.csv',
+    ],
+    # data files containing optionally loaded demonstration data
+    # 'demo': ['demo/demo_data.xml',],
+}
