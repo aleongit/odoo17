5650defa3ceabe49d38d7c135858107769fa8e08
aleongit <aleix.leon@gmail.com>
2025-01-25 16:57:58 +0000

    feat: add a simple card component with props

--- tutorials/awesome_owl/static/src/playground.js
+++ tutorials/awesome_owl/static/src/playground.js
@@ -1,9 +1,10 @@
 /** @odoo-module **/
 
-import { Component, useState } from "@odoo/owl";
+import { Component } from "@odoo/owl";
 import { Counter } from "./counter/counter";
+import { Card } from "./card/card";
 
 export class Playground extends Component {
   static template = "awesome_owl.playground";
-  static components = { Counter };
+  static components = { Counter, Card };
 }
