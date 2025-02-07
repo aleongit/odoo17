ee53576f6900bb986772469f48a577957a0a94ed
aleongit <aleix.leon@gmail.com>
2025-02-07 18:09:47 +0000

    feat: add a todo with event handling

--- tutorials/awesome_owl/static/src/todo_list/todo_list.js
+++ tutorials/awesome_owl/static/src/todo_list/todo_list.js
@@ -8,10 +8,23 @@ export class TodoList extends Component {
   static components = { TodoItem };
 
   setup() {
-    this.todos = useState([
-      { id: 1, description: "write tutorial", isCompleted: false },
-      { id: 2, description: "buy milk", isCompleted: true },
-      { id: 3, description: "save the world", isCompleted: false },
-    ]);
+    // this.todos = useState([
+    //   { id: 1, description: "write tutorial", isCompleted: false },
+    //   { id: 2, description: "buy milk", isCompleted: true },
+    //   { id: 3, description: "save the world", isCompleted: false },
+    // ]);
+    this.todos = useState([]);
+    this.nextId = 0;
+  }
+
+  addTodo(ev) {
+    if (ev.keyCode === 13 && ev.target.value != "") {
+      this.todos.push({
+        id: this.nextId++,
+        description: ev.target.value,
+        isCompleted: false,
+      });
+      ev.target.value = "";
+    }
   }
 }
